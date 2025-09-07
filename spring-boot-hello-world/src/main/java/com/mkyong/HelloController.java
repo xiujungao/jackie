3a73333a2cd2ecfd3fa5dd2e4bdfa22ca3d203df
xiujungao <xiujun@gmail.com>
2025-09-07 14:28:08 -0400

    dfs

--- spring-boot-hello-world/src/main/java/com/mkyong/HelloController.java
+++ spring-boot-hello-world/src/main/java/com/mkyong/HelloController.java
@@ -2,6 +2,7 @@ package com.mkyong;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.slf4j.Logger;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    private static final Path FILE_PATH = Paths.get("/data/file1.txt");
 
     @RequestMapping("/")
     String hello() {
@@ -27,7 +30,10 @@ public class HelloController {
     String readFile() throws IOException {
         // PVC is mounted at /data
         try {
-            String content = Files.readString(Paths.get("/data/file1.txt"));
+            String content = "Hello, this is some text written at " + java.time.LocalDateTime.now();
+            Files.writeString(FILE_PATH, content);
+
+            content = Files.readString(FILE_PATH);
             return "File content: " + content;
         } catch (IOException e) {
             log.error("Failed to read file1.txt from /data", e);
