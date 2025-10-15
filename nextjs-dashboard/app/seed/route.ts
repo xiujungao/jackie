57d12b571b6755aefbc5a615b234471713b72a31
xiujungao <xiujun@gmail.com>
2025-10-15 16:45:08 -0400

    seed data successfully

--- nextjs-dashboard/app/seed/route.ts
+++ nextjs-dashboard/app/seed/route.ts
@@ -110,7 +110,7 @@ export async function GET() {
       seedRevenue(),
     ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
+    return Response.json({ message: 'Database seeded successfully', result }, { status: 200 });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
