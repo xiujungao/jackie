38161b3e44b29f3584139eb4b891020b875a21f6
xiujungao <xiujun@gmail.com>
2025-10-17 21:03:57 -0400

    remove searchParams

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -10,11 +10,12 @@ export default async function Page(
       props: { 
         searchParams?: Promise<{ query?: string; page?: string }>
       }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+  console.log('Invoices Page Props:', props);
+//  const searchParams = await props.searchParams;
+//  const query = searchParams?.query || '';
+//  const currentPage = Number(searchParams?.page) || 1;
 
-  console.log('Invoices Page - query:', query, 'currentPage:', currentPage);
+//  console.log('Invoices Page - query:', query, 'currentPage:', currentPage);
 
   return (
     <div className="w-full">
@@ -22,11 +23,10 @@ export default async function Page(
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
+      <Suspense key={"query" + "currentPage"} fallback={<InvoicesTableSkeleton />}>
+        <Table query={"ra"} currentPage={1} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
         {/* <Pagination totalPages={totalPages} /> */}
