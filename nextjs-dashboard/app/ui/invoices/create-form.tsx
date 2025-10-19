691bf7ef36dc300d92ffda55bb90fb30b57a28eb
xiujungao <xiujun@gmail.com>
2025-10-19 11:15:08 -0400

    amount should be number

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -64,7 +64,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <input
                 id="amount"
                 name="amount"
-//                type="number"
+                type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
