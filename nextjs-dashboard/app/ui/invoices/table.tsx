58214205b0c81c49b1787982035954cf921d36ab
xiujungao <xiujun@gmail.com>
2025-10-20 14:39:52 -0400

    mobile works

--- nextjs-dashboard/app/ui/invoices/table.tsx
+++ nextjs-dashboard/app/ui/invoices/table.tsx
@@ -19,7 +19,6 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {/*
             {invoices?.map((invoice) => (
               <div
                 key={invoice.id}
@@ -55,7 +54,6 @@ export default async function InvoicesTable({
                 </div>
               </div>
             ))}
-            */}
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
