bacd7d37372176168382d1fc64e5b2ed21d3d15e
xiujungao <xiujun@gmail.com>
2025-10-21 17:56:02 -0400

    remove middleware.ts to do not filter on any url

--- nextjs-dashboard/not_middleware.ts
+++ nextjs-dashboard/not_middleware.ts
@@ -1,10 +1,12 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
- 
+
 export default NextAuth(authConfig).auth;
- 
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+    '/((?!api/auth|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.webmanifest|assets|public|.*\\.(?:png|jpg|jpeg|svg|gif|ico|webp|css|js|map)$).*)',
+  ],
   runtime: 'nodejs',
 };
