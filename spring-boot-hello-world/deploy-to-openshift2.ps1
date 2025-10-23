21ff201112e4129eede5569af40ace75c0aee9ad
xiujungao <xiujun@gmail.com>
2025-10-23 15:44:44 -0400

    move hello-world out

--- spring-boot-hello-world/deploy-to-openshift2.ps1
+++
@@ -1,9 +0,0 @@
-# Deploy persistent volume to OpenShift
-# You won't see any content of pv from your application.
-# You can only use pvc to claim the pv.
-oc apply -f .\openshift\pv.yaml
-
-# Deploy persistent volume claim to OpenShift
-oc apply -f .\openshift\pvc.yaml
-
-oc get pods -n jackie --selector app=hello-world -o jsonpath="{range .items[*]}{.metadata.name}{'  SA='}{.spec.serviceAccountName}{'\n'}{end}"
